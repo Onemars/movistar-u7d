cf7f01a8f92406f74e38ab267cde1869de821640
Javier Marcet <javier@marcet.info>
2021-05-03 21:50:40 +0200

    start.sh: check if channel list and guide are present before launching backends, they fail without these files. Try to get them if they do not exist.
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- start.sh
+++ start.sh
@@ -13,6 +13,18 @@ echo "nameserver 127.0.0.1" >> /etc/resolv.conf
 echo "172.26.22.23 www-60.svc.imagenio.telefonica.net" >> /etc/hosts
 echo "172.26.83.49 html5-static.svc.imagenio.telefonica.net" >> /etc/hosts
 
+while ! test -e "${HOME:-/home}/MovistarTV.m3u"; do
+    /app/tv_grab_es_movistartv --m3u "${HOME:-/home}/MovistarTV.m3u"
+    sleep 15
+done
+
+while ! test -e "${HOME:-/home}/guide.xml"; do
+    /app/tv_grab_es_movistartv \
+        --tvheadend "${HOME:-/home}/MovistarTV.m3u" \
+        --output "${HOME:-/home}/guide.xml"
+    sleep 15
+done
+
 ( while (true); do /app/movistar-epg.py; sleep 1; done ) &
 ( while (true); do /app/movistar-u7d.py; sleep 1; done ) &
 
